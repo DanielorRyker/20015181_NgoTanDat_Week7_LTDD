7e0a7461869531abb229f5e50b3e45911f4e76ca
DanielorRyker <hoangraper2017@gmail.com>
2024-10-09 18:46:22 +0700

    Week7

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ShopsNearMe from './screens/ShopsNearMe';
+import DrinksScreen from './screens/DrinksScreen';
+import CartScreen from './screens/CartScreen'; 
+import Home from './screens/Home';
+import { CartProvider } from './CartContext';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CartProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+        name="Home" 
+        component={Home} 
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+        name="ShopsNearMe" 
+        component={ShopsNearMe} 
+        options={{ 
+            headerTitle: '',
+            headerBackTitleVisible: false, 
+            headerStyle: {
+            elevation: 0, 
+            shadowOpacity: 0, 
+            borderBottomWidth: 0, 
+            },
+        header: () => null,
+        }} 
+        />
+        <Stack.Screen 
+        name="DrinksScreen" 
+        component={DrinksScreen} 
+        options={{ 
+            headerTitle: '',
+            headerBackTitleVisible: false, 
+            headerStyle: {
+              elevation: 0, 
+              shadowOpacity: 0, 
+              borderBottomWidth: 0, 
+            },
+        header: () => null,
+        }}
+        />
+        <Stack.Screen 
+        name="CartScreen" 
+        component={CartScreen} 
+        options={{ 
+            headerTitle: '',
+            headerBackTitleVisible: false, 
+            headerStyle: {
+              elevation: 0, 
+              shadowOpacity: 0, 
+              borderBottomWidth: 0, 
+            },
+        header: () => null,
+        }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </CartProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
